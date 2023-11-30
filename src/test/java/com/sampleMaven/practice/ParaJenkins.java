5ac7bb021b959200df242d1421810bf8279f6b48
AbhijithKenchain <Abhijithkenchain@gmail.com>
2023-11-30 12:18:53 +0530

    tdfghj

---
+++ src/test/java/com/sampleMaven/practice/ParaJenkins.java
@@ -0,0 +1,36 @@
+package com.sampleMaven.practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class ParaJenkins {
+	public WebDriver driver;
+	@Test
+	public void paras()
+	{
+		String Browser=System.getProperty("Browser");
+		if (Browser.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		} else if (Browser.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+		} else {
+			System.out.println("invalid browser");
+		}
+		
+	
+		System.out.println("-------------open Browser------------");
+		
+		String Url=System.getProperty("Url");
+		if (Browser.equalsIgnoreCase("chrome")) {
+			driver.get("https://www.google.com");
+		} else if (Browser.equalsIgnoreCase("edge")) {
+			driver.get("https://www.facebook.com");
+		} else {
+			System.out.println("invalid browser");
+		}
+	}
+	}
+
+
